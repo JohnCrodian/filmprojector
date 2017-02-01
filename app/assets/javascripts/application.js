50c4ad09f7045603f4ec9ffbb138a430955329ae
John Crodian <johncrodian24@gmail.com>
2017-02-01 13:15:35 -0500

    Datepicker works. About to start messing with navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
+//= require bootstrap-datepicker
 
-$('.datepicker').datepicker()
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,19 @@ class ApplicationController < ActionController::Base
 
 
   def search 
-    year = params[:search]["date(1i)"]
-    month = params[:search]["date(2i)"]
-    	if month.length < 2
-    		month = "0" + month
-    	end
-    day = params[:search]["date(3i)"]
-    	if day.length < 2
-    		day = "0" + day
-    	end
-    date = "#{year}-#{month}-#{day}"
-    @searchDate = date.to_date.strftime("%m/%d/%y")
-    @screenings = Screening.where("showtime LIKE (?)", date)
+    date = params[:search][:date]
+    year = date.last(4).to_s
+    month = date.first(2).to_s
+    month_remove = date.last(7).to_s
+    day = month_remove.first(2).to_s
+	datebuild = "#{year}" "-" "#{month}" "-" "#{day}"
+	standardDate = datebuild.to_date
+    @searchDate = standardDate.strftime('%A, %B %d')
+    @screenings = Screening.where("showtime LIKE (?)", standardDate)
 
   end
 
-
-
 end
+
+#params are coming in as string month/day/year, i.e. "02/04/2017"
+#standardized date for screening entries is year/month/day "2017-02-04"
