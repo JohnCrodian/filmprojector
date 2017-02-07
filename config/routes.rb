03fa4d521101787336c8c49de796ab9a7d7cf791
John Crodian <johncrodian24@gmail.com>
2017-02-07 10:40:50 -0500

    Fixed problem on routes page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-root 'home#index'
+get '/', do 'home#index'
 get 'application/search', to: 'application#search' 
 post 'screenings/scrape_bryn_mawr'
 post 'screenings/scrape_roxy'
